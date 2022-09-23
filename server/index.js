30bf255911661e4b43af89f21c1b8134a54a0298
TBR <tbr@srsgroup.se>
2022-09-23 05:56:49 +0000

    Remove accidentally added js files

--- server/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var api_1 = require("./api");
-var port = 4000;
-var app = (0, api_1.initApp)();
-app.listen(port, function () { return console.log("API available on port ".concat(port)); });
