094370411f6a6e032abc5dc24866b226b7b15872
TBR <tbr@srsgroup.se>
2022-09-26 06:06:36 +0000

    Handle broken prettier path

--- client/src/components/RedirectFromRoot.tsx
+++ client/src/components/RedirectFromRoot.tsx
@@ -1,19 +1,19 @@
-import { FC, useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { FC, useEffect } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 function useRedirectFromRoot() {
-    const defaultPath = '/posts'
-    const navigate = useNavigate()
-    const location = useLocation()
-    
-    useEffect(() => {
-        if(location.pathname === '/') navigate(defaultPath)
-    }, [location.pathname, navigate])
+	const defaultPath = '/posts'
+	const navigate = useNavigate()
+	const location = useLocation()
+
+	useEffect(() => {
+		if (location.pathname === '/') navigate(defaultPath)
+	}, [location.pathname, navigate])
 }
 
 const RedirectFromRoot: FC = () => {
-    useRedirectFromRoot()
-    return null
+	useRedirectFromRoot()
+	return null
 }
 
 export default RedirectFromRoot
