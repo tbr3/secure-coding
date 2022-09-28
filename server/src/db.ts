f0ce33ab06f9cd1e01d89cd3163e2f5ce5a2a090
TBR <tbr@srsgroup.se>
2022-09-28 13:20:59 +0000

    Replace static db credentials

--- server/src/db.ts
+++ server/src/db.ts
@@ -19,10 +19,10 @@ export type Comment = {
  * DB Configuration.
  */
 export const db = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'root',
-	database: 'demo',
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DB,
 })
 
 db.connect((error: QueryError | null) => {
